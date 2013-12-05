9b750354f9a9669bbba2eb1ec67020497c51205e
Daniel Valencia Backhoff <danvalencia@gmail.com>
2013-12-04 21:05:30 -0800

    Inital setup

---
+++ src/main/java/com/maquinet/CashMonitorDaemon.java
@@ -0,0 +1,12 @@
+package com.maquinet;
+
+/**
+ * @author Daniel Valencia (daniel@tacitknowledge.com)
+ */
+public class CashMonitorDaemon
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
