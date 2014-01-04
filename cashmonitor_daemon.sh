584df7908e97ded67656356d7a513c289641688c
Daniel Valencia Backhoff <danvalencia@gmail.com>
2014-01-04 13:11:51 -0800

    Making bash script print useful information

--- cashmonitor_daemon.sh
+++ cashmonitor_daemon.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-nohup java -jar -Djava.util.logging.config.file=log.properties -Dcashmonitor.properties=cashmonitor.properties target/cash-monitor-daemon-0.1b-jar-with-dependencies.jar 1>/dev/null 2>&1  &
+LOG_PROPERTIES_FILE=log.properties
+CASHMONITOR_PROPERTIES_FILE=cashmonitor.properties
+file_pattern=`grep java.util.logging.FileHandler.pattern < $LOG_PROPERTIES_FILE | cut -d "=" -f 2`
+logs_directory=`dirname $file_pattern`
+echo "Starting up CashMonitor Daemon..."
+nohup java -jar -Djava.util.logging.config.file=$LOG_PROPERTIES_FILE  -Dcashmonitor.properties=$CASHMONITOR_PROPERTIES_FILE bin/cash-monitor-daemon-0.1b-jar-with-dependencies.jar 1>/dev/null 2>&1  &
+echo "CashMonitor Daemon has been started with pid $!"
+echo "Log directory is $logs_directory"
